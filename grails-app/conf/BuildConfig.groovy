6e02bc0a5e9726bcff53a2c2237d32de3232c252
Graeme Rocher <graeme.rocher@gmail.com>
2013-10-15 14:06:21 +0200

    initial update to Grails 2.3.1 snapshot

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,11 +2,27 @@ grails.servlet.version = "2.5" // Change depending on target container complianc
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir="target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.work.dir = "target"
 
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver="maven"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -35,20 +51,20 @@ grails.project.dependency.resolution = {
 		}
 		test("org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion")
 		test("org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion")
-
-		// You usually only need one of these, but this project uses both
 		test "org.codehaus.geb:geb-spock:$gebVersion"
 		test "org.codehaus.geb:geb-junit4:$gebVersion"
+
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
 	}
 	plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.7.1"
+        runtime ":hibernate:3.6.10.1" // or ":hibernate4:4.1.11.1"
+        runtime ":jquery:1.10.2"
         runtime ":resources:1.1.6"
 
-        build ":tomcat:$grailsVersion"
+        build ":tomcat:7.0.42"
 		
-		test ":geb:0.6.3"
-		test ":spock:0.6"
+		test ":geb:0.9.1"
+		
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
